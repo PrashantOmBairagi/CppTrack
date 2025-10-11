dc6cac4812ac693963265dc50dcb3584f10594fc
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-10-11 19:53:15 +0530

    Fixed Zero Case

--- Recursion/functionCall.cpp
+++ Recursion/functionCall.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // WAP using recursion to print factorial of 'n' !!
 int fact(int n) {
-    if(n==1) return 1;
+    if(n==1 || n==0) return 1;
     return n*fact(n-1);
 }
 int main(){
