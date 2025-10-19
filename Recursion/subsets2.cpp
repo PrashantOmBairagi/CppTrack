3a7656345610daeeac4b8e2f8813450c51bd5c86
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-10-19 20:22:07 +0530

    19/10 recursion moderate practice

---
+++ Recursion/subsets2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void print(vector<vector<int>> &arr){
+    for (vector<int> ele : arr) { 
+        cout << "{ ";
+        for (int num : ele) { 
+            cout << num << " ";
+        }
+        cout << "} ";
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void subsets2(vector<int>&arr, int n, vector<int>ans, vector<vector<int>>&finalanswer, bool flag){
+    if(n==0){
+        finalanswer.push_back(ans);
+        return;
+    }
+    if(n<arr.size() && arr[n]==arr[n-1] && !flag){
+        subsets2(arr,n-1,ans,finalanswer,false);
+        return;
+    }
+    subsets2(arr,n-1,ans,finalanswer,false);
+    ans.push_back(arr[n-1]);
+    subsets2(arr,n-1,ans,finalanswer,true);
+    return;
+}
+
+int main(){
+    vector<int>arr ={2,2};
+    //print(arr);
+    vector<vector<int>>finalanswer;
+    vector<int>ans;
+    int n = arr.size();
+    subsets2(arr,n,ans,finalanswer,true);
+
+    
+    print(finalanswer);
+    return 0;
+}
