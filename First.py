e9dc0c8a6d6195e0cbb98320a210369b897b3c66
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-10-31 18:53:24 +0530

    Pushing old python codes

---
+++ First.py
@@ -0,0 +1,7 @@
+print("hii my name is Prashant")
+t=input("Enter the number you want table of :")
+T=int(t)
+print("The Table Of ", T, "follows")
+for i in range(11):
+    j=T*i
+    print(T,"X",i,"=",j)
---
+++ rp_assignment-01.py
@@ -0,0 +1,8 @@
+int_value = 10
+float_value = 5.5
+string_value ="Hello"
+
+print ("Sum:", int_value + float_value )
+print ("Difference:", int_value -float_value )
+print ("Product:", int_value * float_value )
+print ("Quotient:", int_value / float_value )
