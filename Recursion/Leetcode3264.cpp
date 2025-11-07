a92f460e552573e64582efb74cbcbac09048a969
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-11-07 21:26:23 +0530

    07/11 Solved leetcode3264 easy question: and learnt about iterator function of <algorithm> header. which is so cool you can use pointer to address that element and also it is quick

--- Recursion/Leetcode3264.cpp
+++ Recursion/Leetcode3264.cpp
@@ -15,18 +15,20 @@ vector<int> getFinalState(vector<int>& nums, int k,int multiplier) {
     getFinalState(nums,k-1,multiplier);
     return nums;
 }
-vector<int> get(vector<int>& nums, int k, int multiplier) {
+void get(vector<int>& nums, int k, int multiplier) {
         for(int i=0;i<k;i++){
             auto minimum = min_element(nums.begin(),nums.end());
             *minimum *= multiplier;
         }
-        return nums;
+        return;
     }
+
 
 int main(){
     vector<int>nums;
     nums={5,3,1,1};
     int k = 4, multiplier=3;
+    getFinalState(nums,k,multiplier);
     get(nums,k,multiplier);
     for(int ele : nums){
         cout<<ele<<" ";
