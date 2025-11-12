718bec9f34f98db67877ee70038c5e6488fba98f
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-11-12 21:58:28 +0530

    12/11 Implemented cycle sort and reduced its space complexity

--- AdvanceSorting/Leetcode448_FindMissNo.cpp
+++ AdvanceSorting/Leetcode448_FindMissNo.cpp
@@ -2,7 +2,10 @@
 #include<vector>
 using namespace std;
 // DATED : 12-11-2025 : Jabalpur
-// Now there isn't much about the code to learn it is one of the easiest code. will do it with cyclic sorting as well later.
+// Now there isn't much about the code to learn it is one of the easiest code. will do it with cyclic sorting as well later. the time complexity is TC = O(n) but Space complexity is O(n) which can be reduced to O(1) by cyclic sort.
+
+// HERE FROM Line 22 -> 35 What we see is a cyclic sort. it has time complexity of O(n) with no extra space used i.e.space complexity O(1) , except the array which has to be returned; 
+
 vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int>ans;
         int n = nums.size();
@@ -17,9 +20,28 @@ vector<int> findDisappearedNumbers(vector<int>& nums) {
         }
         return ans;
     }
+
+vector<int> findDisappearedNumbers2(vector<int>& nums){
+    int n = nums.size(), i = 0;
+    vector<int>ans;
+    while(i<n){
+        int actIdx = nums[i]-1;
+        if(nums[actIdx]==nums[i]){
+            i++;
+        } else swap(nums[actIdx],nums[i]);
+    }
+    for(int i=0;i<n;i++){
+        if(i+1!=nums[i]) ans.push_back(i+1);
+    }
+    return ans;
+}
 int main(){
     vector<int> nums = {4,3,2,7,8,2,3,1};
-    for(int ele : findDisappearedNumbers(nums)){
+    // for(int ele : findDisappearedNumbers(nums)){
+    //     cout<<ele<<" ";
+    // }  cout<<endl;
+
+    for(int ele : findDisappearedNumbers2(nums)){
         cout<<ele<<" ";
     }  cout<<endl;
 }
