48ac567cd5f6f5bb21a2704eaa64c25f172d876c
Prashant Bairagi <128093445+PrashantOmBairagi@users.noreply.github.com>
2025-11-21 17:27:13 +0530

    Delete rp_assignment-01.py

--- rp_assignment-01.py
+++
@@ -1,8 +0,0 @@
-int_value = 10
-float_value = 5.5
-string_value ="Hello"
-
-print ("Sum:", int_value + float_value )
-print ("Difference:", int_value -float_value )
-print ("Product:", int_value * float_value )
-print ("Quotient:", int_value / float_value )
