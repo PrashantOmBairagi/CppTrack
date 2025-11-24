6016f449d6d629e20ba5dfe87000f5b87b0a36ad
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-11-24 18:18:49 +0530

    24/11 leetcode 2483

---
+++ Advance Algorithms/leetcode2483.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int bestClosingTime(string customers){
+    vector<int>pre(customers.size()+1);
+    int hour = 0;
+    for(int i=0;i<=customers.size();i++){
+        pre[i]=hour;
+        if(customers[i]== 'N') hour++;
+    }
+    hour = 0;
+    // We will apply Postfix sum here.
+    for(int i=customers.size();i>=0;i--){ 
+        if(customers[i]== 'Y') hour++;
+        pre[i] += hour;
+
+    }
+    hour = INT_MAX; int best = 0;
+    for(int i=0;i<customers.size()+1;i++){
+        if(pre[i]<hour){ 
+            hour=pre[i];
+            best=i;
+        } 
+    }
+return best;
+}
+int main (){
+    string s = "NNNNN";
+    cout<<bestClosingTime(s);
+
+    return 0;
+}
