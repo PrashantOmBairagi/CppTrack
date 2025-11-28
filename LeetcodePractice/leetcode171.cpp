8616b6e8a8217c7245d29e7d0d416537d8cb4ee9
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-11-28 20:25:14 +0530

    28/11 Leetcode171 Easy practiced string title to number

---
+++ LeetcodePractice/leetcode171.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+// DATED: 28-11-2025 : Jabalpur : Leetcode 171 EASY
+// Today i solved leetcode 171 which was an easy question cause i have Microcontrollers exam tomorrow. I used bruteforce first and then optimised the solution.
+int titleToNumber(string columnTitle) {
+
+    int n = columnTitle.size();
+    int op = 0;
+
+    for(int i=0;i<n;i++){
+        op += (int(columnTitle[i])-64)*  pow(26,n-i-1);    
+    }
+
+    return op;
+}
+
+int main(){
+    string s = "AAA";
+    cout<<titleToNumber(s);
+}
---
+++ LeetcodePractice/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ int(columnTitle[i])-64
