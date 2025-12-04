153e448e76cdd53f430cb31b4ed35146454f2be2
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2025-12-04 21:29:45 +0530

    04/12 leetcode 168 easy

---
+++ LeetcodePractice/Leetcode168.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// DATED: 04-12-2025 : Jabalpur Leetcode168 EASY
+// Very easy question and easy to implement.
+string convertToTitle(int columnNumber) {
+    string ans = "";
+    while(columnNumber>0){
+        columnNumber--;
+        char ch = (columnNumber%26) + 'A';
+        ans+=ch;
+        columnNumber/=26;
+    }
+    return ans;    
+}
+int main(){
+    int n = 701;
+    cout<<convertToTitle(n);
+}
